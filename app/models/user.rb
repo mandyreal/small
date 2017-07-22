3b553950f42463a1c3419ff3f83e13a3c2259d68
Mandy Real <normandy.real@gmail.com>
2017-07-22 21:37:30 +1000

    Updated user model to validate presence of username and email. added update_access_token! method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :update_access_token!
+
+  validates :username, presence: true
+  validates :email, presence: true
+
+  private
+
+  def update_access_token!
+  	self.access_token = "#{self.id}:#{Devise.friendly_token}"
+  	save
+  end
+
 end
